eef3951347be9c7a6e09b5cd7d3fc0c5b1255d07
Deen16 <113366415+Deen16@users.noreply.github.com>
2023-09-18 10:25:46 +0100

    Create area of triangle code.py

---
+++ area of triangle code.py
@@ -0,0 +1,29 @@
+def triangle_area():
+    base = int(input("please input the length of the base of the triangle"))
+    height = int(input("please input the height of your triangle"))
+    area = base*height/2
+    print ("the area of your triangle is" , area)
+
+triangle_area()
+
+
+def power_calculator():
+    voltage = int(input("please enter the voltage"))
+    current = int(input("please enter the current"))
+    power = current*voltage
+    print("the power is" , power)
+
+power_calculator()
+
+
+def two_numbers():
+    num1 = int(input("please enter your first number"))
+    num2 = int(input("Please enter your second number"))
+    answer = num1+num2
+    print("the answer is" , answer)
+
+two_numbers()
+
+
+
+                 
